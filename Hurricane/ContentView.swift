565e5972e47956cbd55ff5b050f54798dd4d5273
Asya Tealdi <asya2698@live.it>
2022-05-03 12:55:09 +0200

    Initial Commit

---
+++ Hurricane/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hurricane
+//
+//  Created by Asya Tealdi on 03/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hurricane/HurricaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  HurricaneApp.swift
+//  Hurricane
+//
+//  Created by Asya Tealdi on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HurricaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
