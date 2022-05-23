0936c4f6dba7aac37ece114f6b74f4d8ce532824
Asya Tealdi <92304740+AsyaTea@users.noreply.github.com>
2022-05-23 18:13:26 +0200

    Add files via upload

---
+++ Hurricane/View/Statistics/StatisticsViewModel.swift
@@ -0,0 +1,69 @@
+//
+//  StatisticsViewModel.swift
+//  Hurricane
+//
+//  Created by Asya Tealdi on 23/05/22.
+//
+
+
+import Foundation
+import SwiftUI
+
+class StatisticsViewModel : ObservableObject {
+    @Published var selectedTimeFrame = "All time"
+    let timeFrames = ["Per month", "Per 3 months", "Per year" , "All time"]
+    
+    @Published var categoryExpensesList = [ExpenseViewModel]()
+    
+    //Takes current expense list and filters through the given category
+//    func getExpensesCategoryList(expensesList: [ExpenseViewModel], category: Int16) -> [ExpenseViewModel] {
+//        var fetchedList = [ExpenseViewModel]()
+//        fetchedList =  expensesList.map { expense in
+//            if expense.category == category {
+//                fetchedList.append(expense)
+//            }
+//        }
+//
+//    }
+    
+}
+
+struct Category: Hashable {
+    var name: String
+    var color: Color
+    var icon: String
+    var totalCosts: Double
+}
+
+enum CategoryEnum {
+    case mainteinance
+    case fuel
+    case insurance
+    case tolls
+    case fines
+    case parking
+    case other
+    
+}
+
+class CategoryViewModel: ObservableObject {
+    
+     @Published var categories = [Category(name: "Fuel", color: Palette.colorYellow, icon: "fuelType", totalCosts: 20.0),
+                       Category(name: "Mainteinance", color: Palette.colorGreen, icon: "maintanance", totalCosts: 20.0),
+                       Category(name: "Insurance", color: Palette.colorOrange, icon: "insurance", totalCosts: 20.0),
+                       Category(name: "Tolls", color: Palette.colorOrange, icon: "Tolls", totalCosts: 20.0),
+                       Category(name: "Fines", color: Palette.colorOrange, icon: "fines", totalCosts: 20.0),
+                       Category(name: "Parking", color: Palette.colorViolet, icon: "parking", totalCosts: 20.0),
+                       Category(name: "Other", color: Palette.colorViolet, icon: "other", totalCosts: 20.0),
+    ]
+    
+    @Published var currentPickerTab : String = "Overview"
+    
+    //Function to calculate total cost of a category
+    func totalCategoryCost(category: Category) -> Double {
+        
+        return 0.0
+    }
+    
+    
+}
