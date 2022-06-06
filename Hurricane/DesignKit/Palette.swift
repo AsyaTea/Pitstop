6050dff1ed3df738da102977cea77c40ad50c6d9
morbuen <morbuen@live.it>
2022-06-06 17:10:57 +0200

    palette

--- Hurricane/DesignKit/Palette.swift
+++ Hurricane/DesignKit/Palette.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 enum Palette {
     
-    static let white = Color(lightRGB: 0xFFFFFF, darkRGB: 0x1F1F1F)
-    static let black = Color(lightRGB: 0x0B0B0B, darkRGB: 0xFDFDFD)
+    static let white = Color(lightRGB: 0xFFFFFF, darkRGB: 0x1E1E1E)
+    static let black = Color(lightRGB: 0x0B0B0B, darkRGB: 0xF9F9F9)
     
-    static let blackHeader = Color(rgb: 0x0B0B0B)
+    static let blackHeader = Color(rgb: 0x1E1E1E)
     static let whiteHeader = Color(rgb: 0xFFFFFF)
     
     static let colorViolet = Color(rgb: 0xC6B3FF)
@@ -24,10 +24,10 @@ enum Palette {
     
     static let greyHard = Color(lightRGB: 0x616161, darkRGB: 0xA9A9A9)
     static let greyMiddle = Color(lightRGB: 0x8A8A8A, darkRGB: 0x6F6F6F)
-    static let greyInput = Color(lightRGB: 0xD2D2D2, darkRGB: 0x434343)
-    static let greyLight = Color(lightRGB: 0xF5F5F5, darkRGB: 0x0A0909)
+    static let greyInput = Color(lightRGB: 0xD2D2D2, darkRGB: 0x2D2D2D)
+    static let greyLight = Color(lightRGB: 0xF5F5F5, darkRGB: 0x363636)
     static let greyEBEBEB = Color(lightRGB: 0xEBEBEB, darkRGB: 0x353535)
-    static let greyBackground = Color(lightRGB: 0xFBFBFB, darkRGB: 0x2C2C2C)
+    static let greyBackground = Color(lightRGB: 0xFBFBFB, darkRGB: 0x121212)
 
     static let colorVioletLight = Color(rgb: 0x94BCF8)
 
