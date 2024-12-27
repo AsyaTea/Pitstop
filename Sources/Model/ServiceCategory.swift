e45c27613392432c4304196a51766579328aa415
Ivan Voloshchuk <ivo.volosh@gmail.com>
2024-12-27 11:46:06 +0100

    Add: icon and color to ServiceCategory

--- Sources/Model/ServiceCategory.swift
+++ Sources/Model/ServiceCategory.swift
@@ -5,6 +5,8 @@
 //  Created by Ivan Voloshchuk on 27/12/24.
 //
 
+import SwiftUI
+
 enum ServiceCategory: String, Codable, CaseIterable {
     case fuel = "Fuel"
     case maintenance = "Maintenance"
@@ -14,4 +16,46 @@ enum ServiceCategory: String, Codable, CaseIterable {
     case fines = "Fines"
     case parking = "Parking"
     case other = "Other"
+
+    var icon: ImageResource {
+        switch self {
+        case .fuel:
+            .fuel
+        case .maintenance:
+            .maintenance
+        case .insurance:
+            .insurance
+        case .roadTax:
+            .roadTax
+        case .tolls:
+            .tolls
+        case .fines:
+            .fines
+        case .parking:
+            .parking
+        case .other:
+            .other
+        }
+    }
+
+    var color: Color {
+        switch self {
+        case .fuel:
+            Palette.colorYellow
+        case .maintenance:
+            Palette.colorGreen
+        case .insurance:
+            Palette.colorOrange
+        case .roadTax:
+            Palette.colorOrange
+        case .tolls:
+            Palette.colorOrange
+        case .fines:
+            Palette.colorOrange
+        case .parking:
+            Palette.colorViolet
+        case .other:
+            Palette.colorViolet
+        }
+    }
 }
