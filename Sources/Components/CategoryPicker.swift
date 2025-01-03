410dbb7f035674ef4c232ed51d15cc14cef43e5c
Ivan Voloshchuk <keranamv@gmail.com>
2025-01-03 12:07:01 +0100

    Add: categories to category picker init

--- Sources/Components/CategoryPicker.swift
+++ Sources/Components/CategoryPicker.swift
@@ -1,5 +1,5 @@
 //
-//  CustomCategoryPicker2.swift
+//  CategoryPicker.swift
 //  Pitstop-APP
 //
 //  Created by Ivan Voloshchuk on 02/01/25.
@@ -7,19 +7,29 @@
 
 import SwiftUI
 
-// TODO: Rename
-struct CustomCategoryPicker2<T>: View where
+struct CategoryPicker<T>: View where
     T: Hashable & CaseIterable & RawRepresentable & Identifiable,
     T.RawValue == String {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @Binding var selectedCategory: T
+    var categories: [T]
+
+    /// If no category is specified it will take all cases from the enum
+    init(selectedCategory: Binding<T>, categories: [T] = []) {
+        if categories.isEmpty {
+            self.categories = Array(T.allCases)
+        } else {
+            self.categories = categories
+        }
+        _selectedCategory = selectedCategory
+    }
 
     var body: some View {
         ZStack {
             Palette.greyBackground
                 .ignoresSafeArea()
             CustomList {
-                ForEach(Array(T.allCases), id: \.id) { category in
+                ForEach(categories, id: \.id) { category in
                     Button(action: {
                         withAnimation {
                             if selectedCategory != category {
